8e02cac5732299ed4bf48133da23373360c4cff4
Dokuny <nestour95@gmail.com>
2022-07-17 23:37:39 +0900

    project init

---
+++ src/main/java/dokuny/bookdora/BookDoraApplication.java
@@ -0,0 +1,13 @@
+package dokuny.bookdora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookDoraApplication.class, args);
+    }
+
+}
